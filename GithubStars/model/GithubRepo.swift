186ea8f2cb6dedbc2a3757ad9289a6727745c249
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2019-01-29 23:50:29 -0200

    evoluindo

---
+++ GithubStars/model/GithubRepo.swift
@@ -0,0 +1,16 @@
+//
+//  GithubRepo.swift
+//  GithubStars
+//
+//  Created by Alex Rodrigues on 29/01/19.
+//  Copyright © 2019 Alex Rodrigues. All rights reserved.
+//
+
+import Foundation
+
+struct GithubRepo: Codable {
+
+    var name: String?
+    
+    
+}
